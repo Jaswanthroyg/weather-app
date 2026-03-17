ba7953956ecaba4cdc219145c65c5a1bd97ae346
Jaswanth Roy G <jaswanthroy45@gmail.com>
2026-03-17 12:01:44 +0530

    Add files via upload
    First Weather API Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Weather APP Using Python
+This is a simple python project that fetches real-time weather data using openweather API
+## Features
+-Get weather by city name
+-Shows temparature in Celsius
+-Shows weather condtion 
+## Technologies Used
+- Python 
+- Requests library
+- REST API 
+## How To Run
+1. Install requests:
+   pip install requests
+2. Run:
+   python main.py
+
+## Author 
+Jaswanth Roy
---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+api_key="YOUR_API_KEY"
+city=input("Enter city:")
+url="https://api.openweathermap.org/data/2.5/weather"
+params={
+    "q":city,
+    "appid":api_key,
+    "units":"metric"
+    }
+try:
+    response=requests.get(url,params=params)
+    if response.status_code==200:
+        data=response.json()
+        print("City:",data["name"])
+        print("Temparature:",data["main"]["temp"],"°C")
+        print("Weather:",data["weather"][0]["description"])
+    else:
+        print("Error:",response.status_code)
+except requests.exceptions.RequestException as e:
+    print("Request failed:",e)
